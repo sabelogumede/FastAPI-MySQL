2a36a50a16df8566ea805c4cc13366c43b867c1e
Sabelo Gumede <iamsabelogumede@gmail.com>
2025-06-17 19:17:01 +0200

    initial project repository

---
+++ main.py
@@ -0,0 +1,67 @@
+# from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi import FastAPI, HTTPException, Depends, status
+from pydantic import BaseModel
+from typing import Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+# instatiation
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+# used for data validation
+class PostBase(BaseModel):
+    title: str
+    content: str
+    user_id: int
+
+class UserBase(BaseModel):
+    username: str
+
+# database dependencies: try to get our db and eventualy closes connection to db 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+# API Endpoits
+
+@app.post("/posts/", status_code=status.HTTP_201_CREATED)
+async def create_post(post: PostBase, db: db_dependency):
+    db_posts = models.Post(**post.model_dump())
+    db.add(db_posts)
+    db.commit()
+
+@app.delete("/posts/{post_id}", status_code=status.HTTP_200_OK)
+async def delete_post(post_id: int, db: db_dependency):
+    db_post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if db_post is None:
+        raise HTTPException(status_code=404, detail='Post Not found')
+    db.delete(db_post)
+    db.commit()
+
+@app.get("/posts/{post_id}", status_code=status.HTTP_200_OK)
+async def read_post(post_id: int, db: db_dependency):
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if post is None:
+        raise HTTPException(status_code=404, detail='Post not found')
+    return post
+
+@app.post("/users/", status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserBase, db: db_dependency):
+    db_user = models.User(**user.model_dump())
+    db.add(db_user)
+    db.commit()
+
+@app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
+async def read_user(user_id: int, db: db_dependency):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail='User not found')
+    return user
